e6a23b7d30fd7efefc596c031cc6299b8d17199b
kwisek <kwisek@outlook.com>
2024-06-28 22:37:43 +0200

    Initial

---
+++ src/main/java/pl/kwisek/dnd5e/Application.java
@@ -0,0 +1,13 @@
+package pl.kwisek.dnd5e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
